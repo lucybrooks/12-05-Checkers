6cfaf120313e3ede78533a155ba3fff82289c5e4
lucybrooks <lucybrooks@users.noreply.github.com>
2014-12-05 08:04:47 -0700

    Update CheckersMove.java

--- src/CheckersMove.java
+++ src/CheckersMove.java
@@ -1,18 +1,62 @@
-class CheckersMove {
-	// A CheckersMove object represents a move in the game of Checkers.
-	// It holds the row and column of the piece that is to be moved
-	// and the row and column of the square to which it is to be moved.
-	// (This class makes no guarantee that the move is legal.)
+class CheckersMove 
+{
+ private int fromRow;
+ private int fromCol;
+ private int toRow;
+ private int toCol;
 
-	// Position of piece to be moved.
+ // Position of piece to be moved.
 
-	// Square it is to move to.
-	
-	// Constructor.  Just set the values of the instance variables.
-	
-	
-	
-	// Accessors
-	
-	
+ // Square it is to move to.
+ 
+ // Constructor.  Just set the values of the instance variables.
+ 
+ //Constructor
+ public CheckersMove(int fromrow, int fromcol, int torow, int tocol)
+ {
+   fromRow=fromrow;
+   fromCol=fromcol;
+   toRow=torow;
+   toCol=tocol;
+ }
+ 
+ // Accessors
+ public int getFromRow()
+ {
+   return fromRow;
+ }
+ 
+ public int getFromCol()
+ {
+   return fromCol;
+ }
+ 
+ public int getToRow()
+ {
+   return toRow;
+ }
+ 
+ public int getToCol()
+ {
+   return toCol;
+ }
+ 
+ public boolean isJump()
+ {
+   boolean jump=false;
+   if (fromCol-toCol==2)
+   {
+     jump=true;
+     return jump;
+   }
+   else if (fromRow-toRow==2)
+   {
+     jump=true;
+     return jump;
+   }
+   else
+   {
+     return jump;
+   }
+ }
 }   // end class CheckersMove.
